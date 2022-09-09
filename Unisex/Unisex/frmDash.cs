ba861b3fdaf38defcd0d51b50435631da427ce10
Matome <90695344+Matome69@users.noreply.github.com>
2022-09-09 16:46:58 +0200

    Add files via upload

---
+++ Unisex/Unisex/frmDash.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Unisex
+{
+    public partial class frmDash : Form
+    {
+        public frmDash()
+        {
+            InitializeComponent();
+        }
+        public string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\32023960\Desktop\Unisex\Unisex\Unisex\Unisex.mdf;Integrated Security=True";
+        public DataSet ds;
+        public SqlDataAdapter adapter;
+        public SqlConnection cnn;
+
+        private void frmDash_Load(object sender, EventArgs e)
+        {
+            cnn= new SqlConnection(conString);
+            cnn.Open();
+
+            adapter = new SqlDataAdapter();
+            ds = new DataSet();
+            SqlCommand cmd;
+
+
+            String sql = @"SELECT * FROM Client";
+            cmd=new SqlCommand(sql, cnn);
+
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds, "Client");
+
+            dgvClients.DataSource = ds;
+            dgvClients.DataMember = "Client";
+
+            cnn.Close();
+        }
+    }
+}
---
+++ Unisex/Unisex/frmLogin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unisex
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else if (checkBox1.Checked == false)
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+
+        }
+
+        private void lblCreateAcc_Click(object sender, EventArgs e)
+        {
+            frmSignUp signUp = new frmSignUp();
+            this.Hide();
+            signUp.ShowDialog();
+            
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            this.txtPassword.AutoSize = false;
+            this.txtPassword.Size = new System.Drawing.Size(225, 20);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtUsername.Text=="admin101" && txtPassword.Text=="CMPG223")
+            {
+                frmDash dash = new frmDash();
+                
+                dash.ShowDialog();
+                this.Close();
+            }
+        }
+    }
+}
